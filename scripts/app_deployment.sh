1f8f0eda1bf97c9edddcb18ce9dcd744b0a649ec
rrigato <ryanrigato@gmail.com>
2024-09-15 16:20:35 -0500

    app deployment script

--- scripts/app_deployment.sh
+++ scripts/app_deployment.sh
@@ -13,6 +13,10 @@ git add -A
 
 git commit -m "$1"
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
 
 pyenv activate lexicon
 
