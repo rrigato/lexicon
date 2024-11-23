515224f3eefafd7b759b65f49d06d070636b6279
rrigato <ryanrigato@gmail.com>
2024-11-23 16:08:55 -0600

    all unit tests passing

--- tests/test_repo/test_lexicon_repo.py
+++ tests/test_repo/test_lexicon_repo.py
@@ -3,10 +3,10 @@ from unittest.mock import MagicMock, patch
 
 from lexicon.entities.lexicon_entity_model import JapaneseVocabRequest
 
-@patch("lexicon.repo.lexicon_repo.mw")
+
 class TestLexiconRepo(unittest.TestCase):
 
-    def test_create_audio_vocab_card(self, main_window_mock: MagicMock):
+    def test_create_audio_vocab_card(self):
         """audio vocab card created for FlashCard"""
         from fixtures.lexicon_fixtures import mock_flash_cards
         from lexicon.repo.lexicon_repo import create_audio_vocab_card
@@ -17,19 +17,21 @@ class TestLexiconRepo(unittest.TestCase):
         )
 
 
+    @patch("lexicon.repo.lexicon_repo.RotatingFileHandler")
     @patch("lexicon.repo.lexicon_repo.logging")
+    @patch("lexicon.repo.lexicon_repo.mw")
     def test_set_logger(
         self,
         main_window_mock: MagicMock,
-        logging_mock: MagicMock
+        logging_mock: MagicMock,
+        rotating_file_handler_mock: MagicMock
         ):
         """outgoing logging setup called"""
         from lexicon.repo.lexicon_repo import set_logger
 
         set_logger()
 
-        main_window_mock.addonManager.addonsFolder.assert_called_once()
-        logging_mock.handlers.RotatingFileHandler.assert_called_once()
+        rotating_file_handler_mock.assert_called_once()
 
 
     @unittest.skip("skipping")
