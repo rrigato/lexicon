2eb52afc40f132c2b789780d9fc0ad19ece43e45
rrigato <ryanrigato@gmail.com>
2025-06-15 16:29:51 -0500

    Make automatically_generate_definition more modular and add logging

--- lexicon/repo/llm_connector.py
+++ lexicon/repo/llm_connector.py
@@ -7,6 +7,24 @@ from urllib.error import HTTPError
 from lexicon.entities.lexicon_constants import LLM_MODEL_TEMPERATURE, LLM_SYSTEM_PROMPT, OPENAI_API_URL, OPENAI_LLM_MODEL
 from lexicon.entities.lexicon_entity_model import AppConfig, JapaneseVocabRequest
 
+
+def _encoded_openapi_post_data(
+    user_prompt: str,
+) -> bytes:
+    """
+    Encodes the OpenAI API post data
+    """
+    return json.dumps({
+        "model": OPENAI_LLM_MODEL,
+        "messages": [
+            {"role": "system", "content": LLM_SYSTEM_PROMPT},
+            {"role": "user", "content": user_prompt}
+        ],
+        "max_tokens": 100,
+        "temperature": LLM_MODEL_TEMPERATURE
+    }).encode()
+
+
 def automatically_generate_definition(
     app_config: AppConfig,
     japanese_vocab_request: JapaneseVocabRequest
@@ -15,6 +33,7 @@ def automatically_generate_definition(
     Returns a new JapaneseVocabRequest object where only
     the word_definition is populated from an OpenAI API call
     """
+    logging.info(f"automatically_generate_definition - start forming api call")
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {app_config.llm_api_key}"
@@ -26,29 +45,21 @@ def automatically_generate_definition(
         f"{japanese_vocab_request.vocab_to_create}"
     )
 
-    data = {
-        "model": OPENAI_LLM_MODEL,
-        "messages": [
-            {"role": "system", "content": LLM_SYSTEM_PROMPT},
-            {"role": "user", "content": user_prompt}
-        ],
-        "max_tokens": 100,
-        "temperature": LLM_MODEL_TEMPERATURE
-    }
-
     request = Request(
         OPENAI_API_URL,
-        data=json.dumps(data).encode(),
+        data=_encoded_openapi_post_data(
+            user_prompt=user_prompt
+        ),
         headers=headers,
         method="POST"
     )
 
-    logging.info(f"automatically_generate_definition - beginning api call")
+    logging.info(f"automatically_generate_definition - user_prompt: \n {user_prompt}")
 
     try:
         with urlopen(request) as response:
             response_data = json.loads(response.read().decode())
-            logging.info(f"response_data: {response_data}")
+            logging.info(f"automatically_generate_definition - response_data: \n {response_data}")
             word_definition = response_data[
                 "choices"
             ][0]["message"]["content"]
