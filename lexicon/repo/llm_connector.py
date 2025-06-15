1f2205735d3f82846e3ef1140ac95d492e497645
Ryan Rigato <11020780+rrigato@users.noreply.github.com>
2025-06-15 16:16:17 -0500

    Implement OpenAPI call (#42)
    * Implement openapi call
    
    * Manual component test
    
    * openapi call working e2e
    
    * System prompt into application constants (#43)
    
    * Extract system prompt
    
    * lookup_api_definition call in addon init (#44)
    
    * lookup_api_definition call in addon init
    
    * Retrieve AppConfig for making api call
    
    * Minimal validation

--- lexicon/repo/llm_connector.py
+++ lexicon/repo/llm_connector.py
@@ -1,3 +1,10 @@
+import json
+import logging
+import os
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+from lexicon.entities.lexicon_constants import LLM_MODEL_TEMPERATURE, LLM_SYSTEM_PROMPT, OPENAI_API_URL, OPENAI_LLM_MODEL
 from lexicon.entities.lexicon_entity_model import AppConfig, JapaneseVocabRequest
 
 def load_api_definition(
@@ -6,8 +13,61 @@ def load_api_definition(
 ) -> JapaneseVocabRequest:
     """
     Returns a new JapaneseVocabRequest object where only
-    the word_definition is populated
+    the word_definition is populated from an OpenAI API call
     """
-    return JapaneseVocabRequest(
-        word_definition="test_word_definition"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {app_config.llm_api_key}"
+    }
+
+    user_prompt = (
+        "Provide a concise English definition "
+        "for the Japanese word: "
+        f"{japanese_vocab_request.vocab_to_create}"
     )
+
+    data = {
+        "model": OPENAI_LLM_MODEL,
+        "messages": [
+            {"role": "system", "content": LLM_SYSTEM_PROMPT},
+            {"role": "user", "content": user_prompt}
+        ],
+        "max_tokens": 100,
+        "temperature": LLM_MODEL_TEMPERATURE
+    }
+
+    request = Request(
+        OPENAI_API_URL,
+        data=json.dumps(data).encode(),
+        headers=headers,
+        method="POST"
+    )
+
+    logging.info(f"load_api_definition - beginning api call")
+
+    try:
+        with urlopen(request) as response:
+            response_data = json.loads(response.read().decode())
+            logging.info(f"response_data: {response_data}")
+            word_definition = response_data[
+                "choices"
+            ][0]["message"]["content"]
+
+            return JapaneseVocabRequest(
+                word_definition=word_definition
+            )
+    except HTTPError as e:
+        logging.error(f"Error: {e}")
+        raise e
+
+if __name__ == "__main__":
+    api_definition = load_api_definition(
+        AppConfig(
+            llm_api_key=os.getenv("anki_openai_key")
+        ),
+        JapaneseVocabRequest(
+            vocab_to_create="洗脳"
+        )
+    )
+    print(api_definition.word_definition)
+    
