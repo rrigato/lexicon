f9198fb28100b30b3218f831b01e4ca0f098a555
rrigato <ryanrigato@gmail.com>
2025-07-19 13:57:45 -0500

    Upgrade virtualenv dependencies

--- scripts/upgrade_dependencies.sh
+++ scripts/upgrade_dependencies.sh
@@ -1,15 +1,46 @@
-set -e
-
-pip install --upgrade pip
-pip install --upgrade pip-tools
-pip install --upgrade setuptools
 ###########################################
-# Upgrade python requirements/requirements-dev.txt
-# and requirements/requirements-prod.txt
-# Installs requirments-dev.txt
-# TODO - create fresh venv and install
+# Upgrade python dependencies
+# 1. Deactivate any existing lexicon virtualenv
+# 2. Uninstall any existing lexicon virtualenv
+# 3. Create a new lexicon virtualenv
+# 4. Upgrade pip
+# 5. Upgrade pip-tools
+# 6. Upgrade setuptools
+# 7. Upgrade requirements/requirements-prod.txt
+# 8. Upgrade requirements/requirements-dev.txt
 ###########################################
-pip-compile requirements/requirements-prod.in
-pip-compile requirements/requirements-dev.in
+(
+    set -e
 
-pip install --upgrade -r requirements/requirements-dev.txt
+    if [[ "$(pyenv version-name)" == "lexicon" ]]; then
+        pyenv deactivate lexicon
+    fi
+
+    if pyenv virtualenvs | grep -q "lexicon"; then
+        pyenv uninstall lexicon
+    fi
+
+    pyenv virtualenv 3.9 lexicon
+
+
+
+
+    pyenv activate lexicon
+
+    if [[ "$(pyenv version-name)" != "lexicon" ]]; then
+        echo "Error - lexicon virtualenv is not activated"
+        exit 1
+    fi
+    pip install --upgrade pip
+    pip install --upgrade pip-tools
+    pip install --upgrade setuptools
+    ###########################################
+    # Upgrade python requirements/requirements-dev.txt
+    # and requirements/requirements-prod.txt
+    # Installs requirments-dev.txt
+    ###########################################
+    pip-compile requirements/requirements-prod.in
+    pip-compile requirements/requirements-dev.in
+
+    pip install --upgrade -r requirements/requirements-dev.txt
+)
