49c06c929427290dcff05b52aab85cc57ad5bb72
rrigato <ryanrigato@gmail.com>
2025-09-14 10:01:03 -0500

    Extract lookup_api_definition

---
+++ addon/flashcard_creation_orchestrator.py
@@ -0,0 +1,38 @@
+import logging
+from typing import Optional
+
+from lexicon.entities.lexicon_entity_model import JapaneseVocabRequest
+from lexicon.repo.llm_connector import automatically_generate_definition
+from lexicon.repo.lexicon_repo import FlashCardRepo
+
+
+def lookup_api_definition(vocab_word: str) -> str:
+    '''Looks up a word definition using the LLM API
+
+    Args:
+        vocab_word: The vocabulary word to look up
+
+    Returns:
+        The word definition from the API, or empty string
+        if insufficient preconditions are met
+    '''
+    logging.info("lookup_api_definition - starting lookup for vocab_word: %s", vocab_word)
+
+    if vocab_word == "":
+        logging.info("lookup_api_definition - vocab_word is empty")
+        return ""
+
+    addon_app_config = FlashCardRepo.retrieve_app_config()
+
+    if addon_app_config.llm_api_key is None:
+        logging.info("lookup_api_definition - llm_api_key is None")
+        return ""
+
+    '''TODO - Allow user to validate definition before creating
+    flash card'''
+    return automatically_generate_definition(
+        app_config=addon_app_config,
+        japanese_vocab_request=JapaneseVocabRequest(
+            vocab_to_create=vocab_word
+        )
+    ).word_definition
---
+++ tests/test_addon/test_flashcard_creation_orchestrator.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+from addon.flashcard_creation_orchestrator import lookup_api_definition
+from fixtures.lexicon_fixtures import mock_japanese_vocab_request, mock_app_config
+class TestOrchestrateFlashCards(unittest.TestCase):
+
+    @patch("addon.flashcard_creation_orchestrator.automatically_generate_definition")
+    @patch("addon.flashcard_creation_orchestrator.FlashCardRepo")
+    def test_lookup_api_definition(
+        self,
+        FlashCardRepo_mock: MagicMock,
+        automatically_generate_definition_mock: MagicMock,
+        ):
+        """
+        GIVEN
+        - AppConfig.llm_api_key is populated
+        - a populated vocab_word
+
+        WHEN
+            - lookup_api_definition is called
+        THEN
+            - automatically_generate_definition is called
+
+        """
+        app_config_mock = mock_app_config()
+        FlashCardRepo_mock.retrieve_app_config.return_value = app_config_mock
+        automatically_generate_definition_mock.return_value.word_definition = "Mock word definition"
+
+        word_definition = lookup_api_definition(
+            "輩"
+        )
+
+        self.assertEqual(
+            word_definition,
+            "Mock word definition"
+        )
